466207b4e0a483baf05522ce27c5025fe74ec9d6
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-04 14:00:04 +0300

    Initial Commit

---
+++ benzin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  benzin
+//
+//  Created by Александр Болотов on 04.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
