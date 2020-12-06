8ac9d44bd318fdfa0a2f4d41963d7df52a15eaa2
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-07 00:59:14 +0300

    Done: Отправка и ответ сервера, AF.upload

--- benzin/ViewController.swift
+++ benzin/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-
+    
     private var imageData: Data? {
         didSet {
-            imageView.image = UIImage(data: imageData!)
+            print(3)
+            guard imageData != nil else {
+                print(4)
+                return
+            }
+            print(5)
+            if let image = UIImage(data: imageData!) {
+                print(6)
+                imageView.image = image
+            }
         }
     }
     private let urlString = "https://api.benzin.io/v1/removeBackground"
@@ -36,25 +45,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    var mkhj: Data?
     @IBAction func deleteBackground() {
-        imageData = removeBackground(imageData: imageData!)
+        guard imageData != nil else {
+            return
+        }
+        mkhj = removeBackgroundRequest(imageData: imageData!)
     }
     @IBAction func chooseImage() {
-//        let cameraIcon = #imageLiteral(resourceName: "camera")  // image literal
-//        let imageIcon = #imageLiteral(resourceName: "photo")
+        //        let cameraIcon = #imageLiteral(resourceName: "camera")  // image literal
+        //        let imageIcon = #imageLiteral(resourceName: "photo")
         
         let actionSheet = UIAlertController(title: nil,message: nil, preferredStyle: .actionSheet)
         let camera = UIAlertAction(title: "Camera", style: .default) { _ in
             self.chooseImagePicker(source: .camera)
         }
-//        camera.setValue(cameraIcon, forKey: "image")
-//        camera.setValue(CATextLayerAlignmentMode.center, forKey: "titleTextAlignment")
+        //        camera.setValue(cameraIcon, forKey: "image")
+        //        camera.setValue(CATextLayerAlignmentMode.center, forKey: "titleTextAlignment")
         
         let photo = UIAlertAction(title: "Photo", style: .default) { _ in
             self.chooseImagePicker(source: .photoLibrary)
         }
-//        photo.setValue(imageIcon, forKey: "image")
-//        photo.setValue(CATextLayerAlignmentMode.center, forKey: "titleTextAlignment")
+        //        photo.setValue(imageIcon, forKey: "image")
+        //        photo.setValue(CATextLayerAlignmentMode.center, forKey: "titleTextAlignment")
         
         let cancel = UIAlertAction(title: "Cancel", style: .cancel)
         
@@ -65,8 +78,30 @@ class ViewController: UIViewController {
         present(actionSheet, animated: true)
     }
     
-    private func removeBackground(imageData: Data) -> Data {
+    private func removeBackgroundRequest(imageData: Data) -> Data {
         
+        imageView.image = UIImage(data: imageData)
+
+        
+        var multipart = MultipartFormData()
+        multipart.append(imageData, withName: "image_file") // изображение в формате Data
+        let header = HTTPHeader(name: "X-Api-Key", value: key) // заголовок запроса
+        let headers = HTTPHeaders([header])
+
+        
+        AF.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(self.imageView.image!.jpegData(compressionQuality: 0.5)!, withName: "image_file" , fileName: "1", mimeType: "1")
+
+        },
+            to: "https://api.benzin.io/v1/removeBackground", method: .post , headers: headers)
+            .response { resp in
+                debugPrint(resp)
+                
+                self.imageView.image = UIImage(data: resp.data!)
+
+
+        }
         
         return Data()
     }
@@ -74,13 +109,13 @@ class ViewController: UIViewController {
 
 extension ViewController: UINavigationControllerDelegate {
     func chooseImagePicker(source: UIImagePickerController.SourceType) {
-
+        
         if UIImagePickerController.isSourceTypeAvailable(source) {
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self // UINavigationControllerDelegate, UIImagePickerControllerDelegate
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
-
+            
             present(imagePicker, animated: true)
         }
     }
@@ -93,9 +128,9 @@ extension ViewController: UIImagePickerControllerDelegate {
         imageView.clipsToBounds = true
         
         imageData = (info[.editedImage] as? UIImage)?.pngData()
-
-//        imageIsChanged = true
-
+        
+        //        imageIsChanged = true
+        
         dismiss(animated: true)
     }
 }
